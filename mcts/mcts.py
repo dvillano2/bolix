00101a9b8d377cc6a531bc91f7e8a2cc011d23e7
dvillano2 <domvillano@gmail.com>
2026-03-06 16:10:22 -0500

    first draft of exapnd

--- mcts/mcts.py
+++ mcts/mcts.py
@@ -1,6 +1,7 @@
 import torch
 from gameplay.board import Board
 from gameplay.masks import Masks
+from model.model import Model
 
 
 class MCTS:
@@ -10,12 +11,14 @@ class MCTS:
         board: Board,
         masks: Masks,
         total_nodes: int,
+        model: Model,  # typing later
     ):
         self.player: torch.Tensor = plane_states[:, 0, 0, 0]
-        self.current_states: torch.Tensor = plane_states.clone()
-        self.batch_size: int = self.current_states.shape[0]
+        self.plane_states: torch.Tensor = plane_states.clone()
+        self.batch_size: int = self.plane_states.shape[0]
         self.total_nodes: int = total_nodes
         self.total_moves: int = board.height * board.width
+        self.model = model
 
         # POLICIES P in lit
         self.policy = torch.zeros(
@@ -60,7 +63,36 @@ class MCTS:
         )
         # FIRST MOVE ANCESTOR, which of possible moves this node
         # came from, used when rolling over the tree
-        self.parents = torch.zeros(
+        self.first_move_ancestor = torch.zeros(
             [self.batch_size, total_nodes],
             dtype=torch.int32,
         )
+        self.indexer = torch.arange(self.batch_size)
+        self.moves = torch.zeros([self.batch_size], dtype=torch.int)
+
+    def establish_root(self):
+        logits, value = self.model.foward(self.plane_states)
+        self.policy[self.indexer, self.free_index, :] = logits
+        self.free_index += 1
+
+    def expand(self):
+        logits, value = self.model.foward(self.plane_states)
+        self.policy[self.indexer, self.free_index, :] = logits
+        self.parents[self.indexer, self.free_index] = self.current_index
+        # update the first move anscetor
+        parent_first_move = self.first_move_ancestor[
+            self.indexer, self.current_index
+        ]
+        real_first_move = torch.where(
+            self.current_index == 0, self.moves, parent_first_move
+        )
+        self.first_move_ancestor[self.indexer, self.free_index] = (
+            real_first_move
+        )
+
+        ####
+        self.children[self.indexer, self.current_index, self.moves] = (
+            self.free_index
+        )
+        self.current_index = self.free_index
+        self.free_index += 1
