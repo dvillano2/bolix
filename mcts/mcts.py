5509bd5a2a42be05627a84b95622c6255e907aa8
dvillano2 <domvillano@gmail.com>
2026-03-06 16:55:12 -0500

    first draft of backtrack

--- mcts/mcts.py
+++ mcts/mcts.py
@@ -44,9 +44,11 @@ class MCTS:
             - 1
         )
         # PARENTS, set to -1 if none yet
+        # to save the location of the parent and the move taken
+        # to get there
         self.parents = (
             torch.zeros(
-                [self.batch_size, total_nodes],
+                [self.batch_size, total_nodes, 2],
                 dtype=torch.int32,
             )
             - 1
@@ -75,10 +77,11 @@ class MCTS:
         self.policy[self.indexer, self.free_index, :] = logits
         self.free_index += 1
 
-    def expand(self):
-        logits, value = self.model.foward(self.plane_states)
+    def expand(self) -> torch.Tensor:
+        logits, values = self.model.foward(self.plane_states)
         self.policy[self.indexer, self.free_index, :] = logits
-        self.parents[self.indexer, self.free_index] = self.current_index
+        self.parents[self.indexer, self.free_index, 0] = self.current_index
+        self.parents[self.indexer, self.free_index, 1] = self.moves
         # update the first move anscetor
         parent_first_move = self.first_move_ancestor[
             self.indexer, self.current_index
@@ -96,3 +99,17 @@ class MCTS:
         )
         self.current_index = self.free_index
         self.free_index += 1
+        return values
+
+    def backtrack(self, values: torch.Tensor) -> None:
+        parents = self.parents[self.indexer, self.current_index]
+        live_parents = parents[:, 0] >= 0
+        while (live_parents).any():
+            parent_index = parents[:, 0]
+            old_moves = parents[:, 1]
+            self.visits[self.indexer, parent_index, old_moves] += live_parents
+            self.accumulation[self.indexer, parent_index, old_moves] += (
+                live_parents * values
+            )
+            parents = self.parents[self.indexer, parent_index]
+            live_parents = parents[:, 0] >= 0
